b8f0597fc58f585b1e2b6ed0a2f2fbc6c88d1ed9
Jarrod Swords <jarrod.swords@gmail.com>
2025-11-01 20:39:58 -0400

    Add happy path test for ticket creation

--- helpdesk-support-services-spec/source/WhenCreatingATicket.cs
+++ helpdesk-support-services-spec/source/WhenCreatingATicket.cs
@@ -1,4 +1,6 @@
 ﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using MdkLegal.HelpDesk.Support.Domain;
 using MdkLegal.HelpDesk.Support.Infrastructure.Ef;
 using Ticket = MdkLegal.HelpDesk.Support.Domain.Ticket;
 
@@ -6,17 +8,51 @@ namespace MdkLegal.HelpDesk.Support.Services.Spec;
 
 public class WhenCreatingATicket
 {
+    #region Setup
+
+    private readonly CreateTicketHandler _createTicketHandler;
+    private readonly FindTicketHandler _findTicketHandler;
+    private readonly ITicketRepository _repository;
+
+    public WhenCreatingATicket()
+    {
+        _repository = new TicketRepository(new Context());
+        _createTicketHandler = new(_repository);
+    }
+
+    #endregion
+
+    #region Implementation
+
+    public static IEnumerable<object[]> CreateTicketCommands()
+    {
+        yield return [new CreateTicket("Laptop battery request")];
+        yield return [new CreateTicket("Need new security token")];
+    }
+
+    #endregion
+
     #region Requirements
+
+    [Theory]
+    [MemberData(nameof(CreateTicketCommands))]
+    public void ThenTicketIsExpected(CreateTicket command)
+    {
+        var ticket = _createTicketHandler.Handle(command)
+            .Then(ticketId => _findTicketHandler.Handle(new(ticketId))).Value;
+
+        using var scope = new AssertionScope();
+
+        ticket.Title.Should().Be(command.Title);
+    }
 
     [Theory]
     [ClassData(typeof(NullOrWhitespace))]
     public void ThenTitleIsRequired(string invalidTitle)
     {
         var createTicket = new CreateTicket(invalidTitle);
-        var repository = new TicketRepository(new Context());
-        var handler = new CreateTicketHandler(repository);
 
-        var error = handler.Handle(createTicket).Error;
+        var error = _createTicketHandler.Handle(createTicket).Error;
 
         error.Should().Be(Ticket.TitleRequired());
     }
